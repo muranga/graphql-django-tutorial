c55176f52743e6a0dac6da7bb1b7450b7689f02c
James Muranga <jmured@gmail.com>
2017-08-07 16:17:24 -0700

    Adds the new question mutation

--- polls/schema.py
+++ polls/schema.py
@@ -1,5 +1,6 @@
 from graphene_django import DjangoObjectType
 import graphene
+import datetime
 from polls.models import Question
 
 class QuestionType(DjangoObjectType):
@@ -13,4 +14,19 @@ class Query(graphene.ObjectType):
     def resolve_questions(self):
         return Question.objects.all()
 
-schema = graphene.Schema(query=Query)
+class NewQuestion(graphene.relay.ClientIDMutation):
+    class Input:
+        question_text = graphene.String(required=True)
+
+    question = graphene.Field(QuestionType)
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        question_text = input.get("question_text")
+        question = Question.objects.create(question_text=question_text, pub_date=datetime.datetime.now())
+        return NewQuestion(question=question)
+
+class Mutation(graphene.ObjectType):
+    new_question = NewQuestion.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
