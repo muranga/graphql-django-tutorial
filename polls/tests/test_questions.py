3fcdb93e6af0c77e0772a5bc0e0d12a8111c5aaf
James Muranga <jmured@gmail.com>
2017-08-10 10:32:38 -0700

    Adds test for the newQuestion mutation

--- polls/tests/test_questions.py
+++ polls/tests/test_questions.py
@@ -20,3 +20,18 @@ class QuestionsQueryTestCase(TestCase):
         questions = result.data["questions"]
         self.assertEqual(len(questions), 2)
         self.assertEqual(questions[0]["questionText"], "test 1")
+
+    def test_that_user_can_create_question(self):
+        question_text = "what is happening"
+        test_mutation = '''
+        mutation {
+            newQuestion(input: {questionText: "%s"}){
+                question {
+                    id,
+                    questionText
+                }
+            }
+        }
+        ''' % question_text
+        result = schema.execute(test_mutation)
+        self.assertEqual(result.data["newQuestion"]["question"]["questionText"], "what is happening")
